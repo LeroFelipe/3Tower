314e36f1b3c21ee7ddc50bfe1e74bd786681b816
Lerisson <lerofelipe@hotmail.com>
2024-04-17 00:26:21 -0300

    Primeiros passo com electron e UI

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    icon: path.join('./assets/icon.png'),
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+});
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import * as THREE from './node_modules/three/build/three.module.js';
+
 const windowSize = 0.95;
 
 // Configuração da cena, câmera e renderizador
