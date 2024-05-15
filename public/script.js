f1df4bdf7fe8691a7cc90891a8bdb5f380bece02
Lérisson <lerofelipe@hotmail.com>
2024-05-15 15:04:45 -0300

    Função para Criar Torres foi Modulada

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 import * as THREE from './lib/three.module.js';
 import * as OBJLoader from './lib/OBJLoader.js';
+import { createSqrTower } from './lib/sqrTower.js';
 
 const windowSize = 0.95;
 
@@ -19,211 +20,7 @@ document.body.appendChild(renderer.domElement);
 let rotateX = 0;
 let rotateY = 0;
 
-var torre = new THREE.Group();
-
-var h; // para adicionar o terreno em relação a altura da torre 
-
-// Função para criar torre quadrada
-function createSqrTower() {
-
-    var b = 1.8;
-    var c = 0.5;
-    h = 8.5;
-    var h1 = 6.5;
-
-    var y = -h/2;
-    var y2 = h1 - (h/2);
-    
-    var raioCanto = 0.06
-    var raioFace = 0.02;
-    var raioCantoFinal = 0.03
-
-    var incremento = b;
-
-    // Definir as posições dos vértices da torre
-    var vertices = [
-
-        // BASE
-        new THREE.Vector3(b/2, (-h/2), b/2), // A
-        new THREE.Vector3(-b/2, (-h/2), b/2), // B
-        new THREE.Vector3(-b/2, (-h/2), -b/2), // C
-        new THREE.Vector3(b/2, (-h/2), -b/2), // D
-
-        // MEIO 
-        new THREE.Vector3(c/2, (h1-(h/2)), c/2), // A
-        new THREE.Vector3(-c/2, (h1-(h/2)), c/2), // B
-        new THREE.Vector3(-c/2, (h1-(h/2)), -c/2), // C
-        new THREE.Vector3(c/2, (h1-(h/2)), -c/2), // D        
-
-        // TOPO
-        new THREE.Vector3(c/2, (h-(h/2)), c/2), // A
-        new THREE.Vector3(-c/2, (h-(h/2)), c/2), // B
-        new THREE.Vector3(-c/2, (h-(h/2)), -c/2), // C
-        new THREE.Vector3(c/2, (h-(h/2)), -c/2), // D              
-
-    ];
-
-    // Definir as arestas da Torre
-    var edges = [
-
-        // Canto A
-        [0, 4], [4, 8],
-
-        // Canto B
-        [1, 5], [5, 9],
-        
-        // Canto C
-        [2, 6], [6, 10],
-
-        // Canto D        
-        [3, 7], [7, 11]   
-
-    ];
-
-    // Adicionar os cilindros dos cantos à cena
-    for (var i = 0; i < edges.length; i++) {
-        var verticesIndex1 = edges[i][0];
-        var verticesIndex2 = edges[i][1];
-
-        var start = vertices[verticesIndex1];
-        var end = vertices[verticesIndex2];
-
-        if(verticesIndex1 >= 4){
-            cilindro(start, end, raioCantoFinal, raioCantoFinal);
-        }else{
-            cilindro(start, end, raioCanto, raioCantoFinal);
-        };     
-    };
-
-    // Adicionar os cilindros das faces à cena
-    while (y + incremento <= (h1 - (h/2))){
-
-        var x = (b/2) + ((y+(h/2))/h1)*((c-b)/2);
-        var z = x;
-
-        var x1 = (b/2) + (((y+incremento)+(h/2))/h1)*((c-b)/2);
-        var z1 = x1;
-
-        var xk = x/2;
-        var zk = (b/2) + (((y+(incremento/2))+(h/2))/h1)*((c-b)/2);
-
-        var xk1 = (b/2) + (((y+(incremento/2))+(h/2))/h1)*((c-b)/2);
-        var zk1 = x/2;
-
-        var start = new THREE.Vector3(x, y, z);
-        var end = new THREE.Vector3(-x1, y + incremento, z1);
-        var endk = new THREE.Vector3(0, y + incremento, z1);
-        
-        var start1 = new THREE.Vector3(-x, y, z);
-        var end1 = new THREE.Vector3(x1, y + incremento, z1);
-        var endk1 = new THREE.Vector3(-x1, y + incremento, 0);
-
-        var start2 = new THREE.Vector3(-x, y, -z);
-        var end2 = new THREE.Vector3(x1, y + incremento, -z1);
-        var endk2 = new THREE.Vector3(0, y + incremento, -z1);
-
-        var start3 = new THREE.Vector3(x, y, -z);
-        var end3 = new THREE.Vector3(-x1, y + incremento, -z1);
-        var endk3 = new THREE.Vector3(x1, y + incremento, 0);
-
-        var endMeio = new THREE.Vector3(xk, y + incremento/2, zk);
-        var endMeio1 = new THREE.Vector3(-xk, y + incremento/2, zk);
-        var endMeio2 = new THREE.Vector3(-xk1, y + incremento/2, zk1);
-        var endMeio3 = new THREE.Vector3(-xk1, y + incremento/2, -zk1);
-        var endMeio4 = new THREE.Vector3(-xk, y + incremento/2, -zk);
-        var endMeio5 = new THREE.Vector3(xk, y + incremento/2, -zk);
-        var endMeio6 = new THREE.Vector3(xk1, y + incremento/2, -zk1);
-        var endMeio7 = new THREE.Vector3(xk1, y + incremento/2, zk1);
-
-        // FACE A
-        cilindro(start, endk, raioFace, raioFace); // Barra transversal 'direita'
-        cilindro(start1, endk, raioFace, raioFace); // Barra transversal 'esquerda'
-        cilindro(end1, end, raioFace, raioFace); // Barra reta
-        cilindro(end1, endMeio, raioFace, raioFace); // Barra 'K'
-        cilindro(end, endMeio1, raioFace, raioFace); // Barra 'K'
-        cilindro(endMeio, new THREE.Vector3(xk1, y+(incremento/2), zk), raioFace, raioFace); // Barra reta 'K'
-        cilindro(endMeio1, new THREE.Vector3(-xk1, y+(incremento/2), zk), raioFace, raioFace); // Barra reta 'K'
-
-        // FACE B
-        cilindro(start1, endk1, raioFace, raioFace); // Barra transversal 'direita'
-        cilindro(start2, endk1, raioFace, raioFace); // Barra transversal 'esquerda'
-        cilindro(end, end3, raioFace, raioFace); // Barra reta
-        cilindro(end, endMeio2, raioFace, raioFace); // Barra 'K'
-        cilindro(end3, endMeio3, raioFace, raioFace); // Barra 'K'
-        cilindro(endMeio2, new THREE.Vector3(-xk1, y+(incremento/2), zk), raioFace, raioFace); // Barra reta 'K'
-        cilindro(endMeio3, new THREE.Vector3(-xk1, y+(incremento/2), -zk), raioFace, raioFace); // Barra reta 'K'
-
-        // FACE C
-        cilindro(start2, endk2, raioFace, raioFace); // Barra transversal 'direita'
-        cilindro(start3, endk2, raioFace, raioFace); // Barra transversal 'esquerda'
-        cilindro(end3, end2, raioFace, raioFace); // Barra reta
-        cilindro(end3, endMeio4, raioFace, raioFace); // Barra 'K'
-        cilindro(end2, endMeio5, raioFace, raioFace); // Barra 'K'
-        cilindro(endMeio4, new THREE.Vector3(-xk1, y+(incremento/2), -zk), raioFace, raioFace); // Barra reta 'K'
-        cilindro(endMeio5, new THREE.Vector3(xk1, y+(incremento/2), -zk), raioFace, raioFace); // Barra reta 'K'
-
-        // FACE D
-        cilindro(start3, endk3, raioFace, raioFace); // Barra transversal 'direita'
-        cilindro(start, endk3, raioFace, raioFace); // Barra transversal 'esquerda'
-        cilindro(end2, end1, raioFace, raioFace); // Barra reta
-        cilindro(end2, endMeio6, raioFace, raioFace); // Barra 'K'
-        cilindro(end1, endMeio7, raioFace, raioFace); // Barra 'K'
-        cilindro(endMeio6, new THREE.Vector3(xk1, y+(incremento/2), -zk), raioFace, raioFace); // Barra reta 'K'
-        cilindro(endMeio7, new THREE.Vector3(xk1, y+(incremento/2), zk), raioFace, raioFace); // Barra reta 'K'
-
-        y = y + incremento;
-
-        incremento = x1 * 2;
-
-    };
-
-    while (y2 + ((h - h1) / Math.floor(h - h1)) <=  h/2){ // Trecho reto 
-
-        //FACE A
-        cilindro(new THREE.Vector3(-c/2, y2, c/2), new THREE.Vector3(c/2, y2 +((h - h1) / Math.floor(h - h1)), c/2), raioFace, raioFace);
-        cilindro(new THREE.Vector3(c/2, y2, c/2), new THREE.Vector3(-c/2, y2 +((h - h1) / Math.floor(h - h1)), c/2), raioFace, raioFace);
-        cilindro(new THREE.Vector3(-c/2, y2 +((h - h1) / Math.floor(h - h1)), c/2), new THREE.Vector3(c/2, y2 +((h - h1) / Math.floor(h - h1)), c/2), raioFace, raioFace);
-
-        //FACE B
-        cilindro(new THREE.Vector3(-c/2, y2, c/2), new THREE.Vector3(-c/2, y2 +((h - h1) / Math.floor(h - h1)), -c/2), raioFace, raioFace);
-        cilindro(new THREE.Vector3(-c/2, y2, -c/2), new THREE.Vector3(-c/2, y2 +((h - h1) / Math.floor(h - h1)), c/2), raioFace, raioFace);
-        cilindro(new THREE.Vector3(-c/2, y2 +((h - h1) / Math.floor(h - h1)), -c/2), new THREE.Vector3(-c/2, y2 +((h - h1) / Math.floor(h - h1)), c/2), raioFace, raioFace);
-
-        //FACE C
-        cilindro(new THREE.Vector3(-c/2, y2, -c/2), new THREE.Vector3(c/2, y2 +((h - h1) / Math.floor(h - h1)), -c/2), raioFace, raioFace);
-        cilindro(new THREE.Vector3(c/2, y2, -c/2), new THREE.Vector3(-c/2, y2 +((h - h1) / Math.floor(h - h1)), -c/2), raioFace, raioFace);
-        cilindro(new THREE.Vector3(c/2, y2 +((h - h1) / Math.floor(h - h1)), -c/2), new THREE.Vector3(-c/2, y2 +((h - h1) / Math.floor(h - h1)), -c/2), raioFace, raioFace);
-
-        //FACE D
-        cilindro(new THREE.Vector3(c/2, y2, -c/2), new THREE.Vector3(c/2, y2 +((h - h1) / Math.floor(h - h1)), c/2), raioFace, raioFace);
-        cilindro(new THREE.Vector3(c/2, y2, c/2), new THREE.Vector3(c/2, y2 +((h - h1) / Math.floor(h - h1)), -c/2), raioFace, raioFace);
-        cilindro(new THREE.Vector3(c/2, y2 +((h - h1) / Math.floor(h - h1)), c/2), new THREE.Vector3(c/2, y2 +((h - h1) / Math.floor(h - h1)), -c/2), raioFace, raioFace);
-
-
-        y2 = y2 + ((h - h1) / Math.floor(h - h1));
-
-    }; 
-
-}
-
-function cilindro(start, end, raioFinal, raio){
-    var edgeVector = end.clone().sub(start);
-    var edgeLength = edgeVector.length();
-
-    var cylinderMaterial = new THREE.MeshStandardMaterial({ color: 0xffa500 });
-    var edgeGeometry = new THREE.CylinderGeometry(raio, raioFinal, edgeLength, 16);
-    var edge = new THREE.Mesh(edgeGeometry, cylinderMaterial);
-
-    edge.position.set((start.x + end.x)/2 , (start.y + end.y)/2 , (start.z + end.z)/2 );
-
-    // Calcular a orientação do cilindro
-    edge.quaternion.setFromUnitVectors(new THREE.Vector3(0, 1, 0), edgeVector.clone().normalize());
-    
-    edge.castShadow = true; // Permitir que o cilindro emita sombras
-    edge.receiveShadow = true; // Permitir que o cilindro receba sombras
-
-    torre.add(edge);
-}
+var h = 8.5; // para adicionar o terreno em relação a altura da torre 
 
 function planeXY(){
     // Definir as dimensões do plano
@@ -277,7 +74,8 @@ function xyzLines(){
     };
 }
 
-createSqrTower();
+var torre = createSqrTower(1.8, 0.5, h, 6.5, 0.06, 0.02, 0.03);
+
 scene.add(torre);
 //xyzLines();
 //planeXY();
